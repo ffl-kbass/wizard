0ef062a1a9ef136b4e99067cd4afefd4a9b91a19
kbass-ffl <kbass@fundsforlearning.com>
2021-08-27 07:44:09 -0500

    directory changes

--- pages/SPI.tsx
+++ pages/SPI.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import SPIWizard from '../Components/SPI/Wizard'
+import Wizard from '../Components/Wizard'
 import ServiceInformation from '../Components/SPI/Wizard/ServiceInformation';
 import Contact from '../Components/SPI/Wizard/Contact';
 import ApplicantPayment from '../Components/SPI/Wizard/ApplicantPayment';
@@ -11,14 +11,14 @@ export default function Home() {
         <Head>
           <title>Create Next App</title>
         </Head>
-        <SPIWizard
+        <Wizard
           stageTitles={['Service Information','Contact','Applicant Payment','Invoice Number']}
         >
           <ServiceInformation/>
           <Contact />
           <ApplicantPayment/>
           <InvoiceNumber/>
-        </SPIWizard>
+        </Wizard>
       </>
   )
 }
